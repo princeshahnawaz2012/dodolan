3e7f3e8b2b6cdb2a42a10c3c4eeebbf00cfa2823
Zidni Mubarock <zidmubarock@gmail.com>
2011-04-07 12:37:28 +0700

    autoloading Misc Library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('theme', 'asset', 'database', 'session', 'messages', 'addon_store', 'yh_conv', 'addon_store', 'cart', 'bug');
+$autoload['libraries'] = array('theme', 'asset', 'database', 'session', 'messages', 'addon_store', 'yh_conv', 'addon_store', 'cart', 'bug', 'misc');
 
 
 /*
