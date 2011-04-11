a1918540b467e89e7020e791217702fb7ab3b110
Zidni Mubarock <zidmubarock@gmail.com>
2011-04-11 13:10:40 +0700

    adding third parameter to render function, as paramater optional to choose where mainLayer to be render.

--- application/libraries/Theme.php
+++ application/libraries/Theme.php
@@ -10,13 +10,19 @@ function theme(){
 	$this->_ci =& get_instance();
 }
 
-function render($data, $mode=false){
+function render($data, $mode=false, $layer=false){
+	if($layer == false){
+		$layer = 'index';
+	}else{
+		$layer = $layer;
+	}
+	
 	if($mode != 'back' && $mode == null ){
-	$rend = $this->_ci->load->view('front/index', $data);
+	$rend = $this->_ci->load->view('front/'.$layer, $data);
 	return $rend;
 	}
 	else{
-	$rend = $this->_ci->load->view('back/index', $data);
+	$rend = $this->_ci->load->view('back/'.$layer, $data);
 	return $rend;	
 	}
 }
