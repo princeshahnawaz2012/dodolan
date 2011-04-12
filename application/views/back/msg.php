18bd15b4640630ede131c3c3e5799205797502d4
Zidni Mubarock <zidmubarock@gmail.com>
2011-04-12 20:21:18 +0700

    remove $this->messages->get() directly on view,

--- application/views/back/msg.php
+++ application/views/back/msg.php
@@ -1,18 +1,12 @@
-
 <div class="msg-Ui">
 	<?php
 	// display all messages
-	$messages = $this->messages->get();
-	
-	if (is_array($messages)):
-	    foreach ($messages as $type => $msgs):
+	    foreach ($msg as $type => $msgs):
 	        foreach ($msgs as $message):
 	            echo ('<div class="msg-item '.$type.'">
 					  <span class="close"></span>'.$message.'</div>');
-	        endforeach;
+	       endforeach;
 	    endforeach;
-	endif;
-
-	?>	
-
+	?>
 </div>
+<div class="clear"></div>
