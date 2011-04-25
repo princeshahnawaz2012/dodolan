45117b514936f51c1683070c663e255be7c979c9
Zidni Mubarock <zidmubarock@gmail.com>
2011-04-26 00:36:35 +0700

    Start Adding UPS Library

---
+++ application/libraries/ups.php
@@ -0,0 +1,21 @@
+<?
+#doc
+#    classname:    Ups
+#    scope:        PUBLIC
+#
+#/doc
+
+class Ups 
+{
+    #    internal variables
+    
+    #    Constructor
+    function __construct (argument)
+    {
+        # code...
+        
+    }
+    ###    
+
+}
+###
