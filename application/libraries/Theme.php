b3551d2ab635e3822904793bd7552834d054aea1
Zidni Mubarock <zidmubarock@gmail.com>
2011-05-27 03:24:51 +0000

    adding ckeditor generate function

--- application/libraries/Theme.php
+++ application/libraries/Theme.php
@@ -45,5 +45,52 @@ function menu_rend($source){
 	
 	
 }
+function load_text_editor($id){
+	$this->_ci->load->helper('url');
+	$this->_ci->load->helper('ckeditor');
+	//Ckeditor's configuration
+	$config = array(
+	//ID of the textarea that will be replaced
+				'id' 	=> 	$id,
+				'path'	=>	'assets/global_js/ckeditor',
+
+				//Optionnal values
+				'config' => array(
+					'toolbar' 	=> 	"Full", 	//Using the Full toolbar
+					'width' 	=> 	"100%",	//Setting a custom width
+					'height' 	=> 	'200px',	//Setting a custom height
+
+				),
+
+				//Replacing styles from the "Styles tool"
+				'styles' => array(
+
+					//Creating a new style named "style 1"
+					'style 1' => array (
+						'name' 		=> 	'Blue Title',
+						'element' 	=> 	'h2',
+						'styles' => array(
+							'color' 	=> 	'Blue',
+							'font-weight' 	=> 	'bold'
+						)
+					),
+
+					//Creating a new style named "style 2"
+					'style 2' => array (
+						'name' 	=> 	'Red Title',
+						'element' 	=> 	'h2',
+						'styles' => array(
+							'color' 		=> 	'Red',
+							'font-weight' 		=> 	'bold',
+							'text-decoration'	=> 	'underline'
+						)
+					)				
+				)
+			);
+		
+		echo display_ckeditor($config);
+
+		}
+
 
 }
