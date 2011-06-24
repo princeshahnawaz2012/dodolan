05aac4a889b97583eae8cbbc1d6c5ee2134664cf
Zidni Mubarock <zidmubarock@gmail.com>
2011-06-24 06:51:31 +0000

    Change function run for call widget to Placed

--- application/modules/backend/views/sample_view_admin.php
+++ application/modules/backend/views/sample_view_admin.php
@@ -2,5 +2,5 @@
 	<?=modules::run('backend/widget/main_statistic')?>
 
 <br class="clear"/>
-	<?php widget_helper::run('admin/admin_ga/admin_ga'); ?>  
+	<?php widget_helper::placed('admin/admin_ga/admin_ga'); ?>  
 </div>
